fd702c9600bea94f36ed1192eb419431aeeddfa2
jonyz <jvalente96@gmail.com>
2022-07-06 02:27:02 +0100

    fixed links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,25 @@ export default function Footer() {
   return (
     <div>
       <footer className={styles.footer}>
-        <a href="http://discord.gg/hWyBHrUDAk" target="_blank">
+        <a
+          href="http://discord.gg/hWyBHrUDAk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Icon fill="#000000" size={32} svg="discord" />
         </a>
-        <a href="https://twitter.com/lenftapp" target="_blank">
+        <a
+          href="https://twitter.com/lenftapp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Icon fill="#000000" size={32} svg="twitter" />
         </a>
-        <a href="https://github.com/leNFT" target="_blank">
+        <a
+          href="https://github.com/leNFT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Icon fill="#000000" size={32} svg="github" />
         </a>
       </footer>

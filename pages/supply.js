56be44a974541ad60ced1bcc7366afcd8d3d35c5
jonyz <jvalente96@gmail.com>
2022-07-06 16:03:41 +0100

    added balance

--- pages/supply.js
+++ pages/supply.js
@@ -1,13 +1,72 @@
 import styles from "../styles/Home.module.css";
 import { Button, Modal } from "web3uikit";
 import ReserveDetails from "../components/ReserveDetails";
+import contractAddresses from "../contractAddresses.json";
+import { useState, useEffect } from "react";
 import Deposit from "../components/Deposit";
 import Withdraw from "../components/Withdraw";
-import { useState } from "react";
+import marketContract from "../contracts/Market.json";
+import reserveContract from "../contracts/Reserve.json";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { formatUnits, parseUnits } from "@ethersproject/units";
 
 export default function Supply() {
   const [visibleDepositModal, setVisibleDepositModal] = useState(false);
   const [visibleWithdrawalModal, setVisibleWithdrawalModal] = useState(false);
+  const { isWeb3Enabled, chainId, account } = useMoralis();
+  const [maxAmount, setMaxAmount] = useState("0");
+  const addresses =
+    chainId in contractAddresses
+      ? contractAddresses[chainId]
+      : contractAddresses["0x1"];
+
+  const { runContractFunction: getReserveAddress } = useWeb3Contract({
+    abi: marketContract.abi,
+    contractAddress: addresses.Market,
+    functionName: "getReserveAddress",
+    params: {
+      asset: addresses.WETH,
+    },
+  });
+
+  const { runContractFunction: getMaximumWithdrawalAmount } = useWeb3Contract();
+
+  async function updateMaxAmount() {
+    const reserveAddress = (await getReserveAddress()).toString();
+
+    const maxWithdrawalOptions = {
+      abi: reserveContract.abi,
+      contractAddress: reserveAddress,
+      functionName: "getMaximumWithdrawalAmount",
+      params: {
+        to: account,
+      },
+    };
+
+    const updatedMaxAmount = (
+      await getMaximumWithdrawalAmount({
+        params: maxWithdrawalOptions,
+      })
+    ).toString();
+
+    console.log("Updated Max Withdrawal Amount:", updatedMaxAmount);
+    setMaxAmount(updatedMaxAmount);
+  }
+
+  //Run once
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateMaxAmount();
+    }
+  }, [isWeb3Enabled]);
+
+  //Run once
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateMaxAmount();
+    }
+  }, [isWeb3Enabled]);
+
   return (
     <div className={styles.container}>
       <Modal
@@ -32,29 +91,36 @@ export default function Supply() {
       </Modal>
       <div className="flex items-center justify-center">
         <div className="flex flex-col items-center m-16">
-          <div className="m-4">
-            <Button
-              text="Deposit"
-              theme="colored"
-              type="button"
-              size="large"
-              color="blue"
-              onClick={async function () {
-                setVisibleDepositModal(true);
-              }}
-            ></Button>
+          <div className="flex flex-row">
+            <div className="m-4">
+              <Button
+                text="Deposit"
+                theme="colored"
+                type="button"
+                size="large"
+                color="blue"
+                radius="5"
+                onClick={async function () {
+                  setVisibleDepositModal(true);
+                }}
+              />
+            </div>
+            <div className="m-4">
+              <Button
+                text="Withdraw"
+                theme="colored"
+                type="button"
+                size="large"
+                color="blue"
+                radius="5"
+                onClick={async function () {
+                  setVisibleWithdrawalModal(true);
+                }}
+              />
+            </div>
           </div>
-          <div className="m-4">
-            <Button
-              text="Withdraw"
-              theme="colored"
-              type="button"
-              size="large"
-              color="blue"
-              onClick={async function () {
-                setVisibleWithdrawalModal(true);
-              }}
-            ></Button>
+          <div className="flex flex-row">
+            Reserve Balance: {formatUnits(maxAmount, 18)} WETH
           </div>
         </div>
         <div className="flex flex-col items-center m-16">

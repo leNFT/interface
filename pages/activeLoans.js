3359cde8c8c09011c5f93cd3b5b4706dc357f9bd
jonyz <jvalente96@gmail.com>
2022-07-10 06:48:36 +0100

    add loading to active loans

--- pages/activeLoans.js
+++ pages/activeLoans.js
@@ -5,11 +5,12 @@ import loanCenterContract from "../contracts/LoanCenter.json";
 import erc721 from "../contracts/erc721.json";
 import { useMoralisWeb3Api, useWeb3Contract, useMoralis } from "react-moralis";
 import { useState, useEffect } from "react";
-import { Card, Tooltip, Illustration, Modal, Typography } from "web3uikit";
+import { Card, Tooltip, Illustration, Loading, Typography } from "web3uikit";
 import Image from "next/image";
 
 export default function ActiveLoans() {
   const [activeLoans, setActiveLoans] = useState([]);
+  const [loadingActiveLoans, setLoadingActiveLoans] = useState(true);
   const { isWeb3Enabled, chainId, account } = useMoralis();
   const addresses =
     chainId in contractAddresses
@@ -100,6 +101,7 @@ export default function ActiveLoans() {
     }
     // Update active loans state array
     setActiveLoans(updatedActiveLoans);
+    setLoadingActiveLoans(false);
   }
 
   // Runs once
@@ -113,36 +115,42 @@ export default function ActiveLoans() {
   return (
     <div className={styles.container}>
       <div className="flex flex-row items-center justify-center">
-        {activeLoans.length != 0 && (
-          <div id="activeLoansContainer" className="flex m-2 p-2">
-            {activeLoans.map((activeLoan) => (
-              <div key={activeLoan.loanId} className="m-4">
-                <Card title={"#" + activeLoan.loanId}>
-                  <div className="p-2">
-                    {activeLoan.tokenURI ? (
-                      <div className="flex flex-col items-end gap-2">
-                        <Image
-                          loader={() => activeLoan.tokenURI}
-                          src={activeLoan.tokenURI}
-                          height="140"
-                          width="140"
-                        />
-                      </div>
-                    ) : (
-                      <div className="flex flex-col items-center gap-1">
-                        <Illustration
-                          height="140px"
-                          logo="chest"
-                          width="100%"
-                        />
-                        Loading...
-                      </div>
-                    )}
-                  </div>
-                </Card>
-              </div>
-            ))}
+        {loadingActiveLoans ? (
+          <div className="flex m-16">
+            <Loading size={16} spinnerColor="#2E7DAF" spinnerType="wave" />
           </div>
+        ) : (
+          activeLoans.length != 0 && (
+            <div id="activeLoansContainer" className="flex m-2 p-2">
+              {activeLoans.map((activeLoan) => (
+                <div key={activeLoan.loanId} className="m-4">
+                  <Card title={"#" + activeLoan.loanId}>
+                    <div className="p-2">
+                      {activeLoan.tokenURI ? (
+                        <div className="flex flex-col items-end gap-2">
+                          <Image
+                            loader={() => activeLoan.tokenURI}
+                            src={activeLoan.tokenURI}
+                            height="140"
+                            width="140"
+                          />
+                        </div>
+                      ) : (
+                        <div className="flex flex-col items-center gap-1">
+                          <Illustration
+                            height="140px"
+                            logo="chest"
+                            width="100%"
+                          />
+                          Loading...
+                        </div>
+                      )}
+                    </div>
+                  </Card>
+                </div>
+              ))}
+            </div>
+          )
         )}
       </div>
     </div>

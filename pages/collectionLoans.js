7ce67026c9638fe963c5f3bfc670f71df7fe7035
jonyz <jvalente96@gmail.com>
2022-08-02 20:02:18 +0100

    update dependencies

--- pages/collectionLoans.js
+++ pages/collectionLoans.js
@@ -200,6 +200,10 @@ export default function CollectionLoans() {
     const maxCollateralNumber = BigNumber.from(maxCollateralString);
     const debtNumber = BigNumber.from(debtString);
 
+    console.log("Calculation health level...");
+    console.log("Debt:", debtString);
+    console.log("Max Collateral:", maxCollateralString);
+
     return maxCollateralNumber
       .sub(debtNumber)
       .mul(BigNumber.from(100))

ed1e25fea3dc8b85a0f231b32b33caa6f7488906
jonyz <jvalente96@gmail.com>
2022-08-07 04:10:17 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    updateUI();
-  }, []);
-
   return (
     <div className={styles.container}>
       <div className={styles.main}>

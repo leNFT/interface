e87c665545c57c67538718357350c8d9a17ee75c
jonyz <jvalente96@gmail.com>
2022-08-07 03:56:34 +0100

    remove borrow rate from landing page

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,8 @@
 import styles from "../styles/Home.module.css";
-import reserveContract from "../contracts/Reserve.json";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useMoralisWeb3Api, useMoralisWeb3ApiCall } from "react-moralis";
+import { useEffect } from "react";
 
 export default function Home() {
-  const [borrowRate, setBorrowRate] = useState(0);
-  const { native } = useMoralisWeb3Api();
-
-  const options = {
-    chain: "goerli",
-    address: "0x82146b8c9F6aDcc4af851561ebb59195e34e10Bf",
-    function_name: "getBorrowRate",
-    abi: reserveContract.abi,
-    params: {},
-  };
-
-  const { fetch } = useMoralisWeb3ApiCall(native.runContractFunction, {
-    ...options,
-  });
-
-  async function updateUI() {
-    let updatedBorrowRate = await fetch();
-    console.log("updatedBorrowRate", updatedBorrowRate);
-    setBorrowRate(updatedBorrowRate);
-  }
-
   useEffect(() => {
     updateUI();
   }, []);
@@ -82,9 +59,6 @@ export default function Home() {
         <div className="flex flex-row m-16 max-w-lg">
           leNFT is a peer-to-pool NFT lending market. it allows you to get
           instant liquidity using your NFTs as collateral
-        </div>
-        <div className="flex flex-row m-16">
-          current borrow rate is {borrowRate / 100} %
         </div>
       </div>
     </div>

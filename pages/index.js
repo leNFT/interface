ab89c5bdc818a9f3400ecb222e067f545f7ee6bd
jonyz <jvalente96@gmail.com>
2022-08-17 23:23:36 +0100

    update button and bg image

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
                     <Button
                       primary
                       size="medium"
-                      color="silver"
+                      color="black"
                       label={
                         <Box
                           sx={{

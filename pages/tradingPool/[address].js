134e4b2c95a337ec4d2c7aeecd04c8a0ba81f034
jvalente <jvalente96@gmail.com>
2022-12-18 15:55:30 +0000

    .

--- pages/tradingPool/[address].js
+++ pages/tradingPool/[address].js
@@ -1,46 +1,7 @@
-import styles from "../styles/Home.module.css";
-import contractAddresses from "../contractAddresses.json";
-import { getAssetPrice } from "../helpers/getAssetPrice.js";
-import { getNFTImage } from "../helpers/getNFTImage.js";
-import { getAddressNFTs } from "../helpers/getAddressNFTs.js";
-import { getSupportedNFTs } from "../helpers/getSupportedNFTs.js";
 import { useState, useEffect } from "react";
-import Link from "next/link";
-import Pagination from "@mui/material/Pagination";
-import { useNotification, Tooltip, Loading, Input } from "@web3uikit/core";
-import { HelpCircle, Search } from "@web3uikit/icons";
-import { BigNumber } from "@ethersproject/bignumber";
-import Borrow from "../components/Borrow";
-import RepayLoan from "../components/RepayLoan";
-import Image from "next/image";
-import { getAddress } from "@ethersproject/address";
-import loanCenterContract from "../contracts/LoanCenter.json";
-import { calculateHealthLevel } from "../helpers/healthLevel.js";
-import LinearProgressWithLabel from "../components/LinearProgressWithLabel";
-import StyledModal from "../components/StyledModal";
-import { Divider } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { CardActionArea } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import { useAccount, useNetwork, useContract, useProvider } from "wagmi";
 
 export default function Lend() {
-  const SEARCH_PAGE_SIZE = 8;
-  const [loadingUI, setLoadingUI] = useState(true);
-  const [loans, setLoans] = useState([]);
-  const [supportedAssets, setSupportedAssets] = useState([]);
-  const [searchPage, setSearchPage] = useState(0);
-  const [searchResults, setSearchResults] = useState([]);
-  const [searchPageData, setSearchPageData] = useState([]);
-  const [searchInputString, setSearchInputString] = useState("");
-
-  const [unsupportedAssets, setUnsupportedAssets] = useState([]);
-  const [visibleAssetModal, setVisibleAssetModal] = useState(false);
-  const [visibleLoanModal, setVisibleLoanModal] = useState(false);
-  const [selectedAsset, setSelectedAsset] = useState();
-  const [selectedLoan, setSelectedLoan] = useState();
   const { address, isConnected } = useAccount();
   const { chain } = useNetwork();
 

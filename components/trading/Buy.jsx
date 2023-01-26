297440e513b82a94322a267112597287a7b2b2fc
jvalente <jvalente96@gmail.com>
2023-01-26 09:50:30 +0000

    fix buy exact

--- components/trading/Buy.jsx
+++ components/trading/Buy.jsx
@@ -419,7 +419,7 @@ export default function Buy() {
                       } else {
                         newSelectedNFTs.splice(index, 1);
                       }
-                      getBuySelectedPriceQuote(newSelectedNFTs);
+                      getPriceQuote(newSelectedNFTs.length);
                     }}
                   >
                     <CardContent>
--- helpers/getBuyExactQuote.js
+++ helpers/getBuyExactQuote.js
@@ -18,10 +18,10 @@ export async function getBuyExactQuote(chainId, nfts, pool) {
     "&chainId=" +
     chainId;
   console.log(requestURL);
-  const buyAnyQuoteResponse = await fetch(requestURL, options).catch((err) =>
+  const buyExactQuoteResponse = await fetch(requestURL, options).catch((err) =>
     console.error(err)
   );
-  const buyAnyQuote = await buyAnyQuoteResponse.json();
+  const buyExactQuote = await buyExactQuoteResponse.json();
 
-  return buyAnyQuote;
+  return buyExactQuote;
 }

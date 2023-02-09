3a531817a232c2d48ed84c8e648113321f886aab
jvalente <jvalente96@gmail.com>
2023-02-09 04:24:02 +0000

    update curve pool

--- helpers/getNativeTokenETHPrice.js
+++ helpers/getNativeTokenETHPrice.js
@@ -1,8 +1,28 @@
-import fetch from "node-fetch";
+import contractAddresses from "../contractAddresses.json";
+import curvePoolContract from "../contracts/CurvePool.json";
+import {
+  useAccount,
+  useProvider,
+  useNetwork,
+  useContract,
+  useSigner,
+} from "wagmi";
 
 export async function getNativeTokenETHPrice(chainId) {
-  const serverAddress = "https://lenft-api-w27ha.ondigitalocean.app";
+  const provider = useProvider();
   var price = "0";
 
-  return price;
+  const curvePoolProvider = useContract({
+    contractInterface: curvePoolContract.abi,
+    addressOrName: contractAddresses[chainId].CurvePool,
+    signerOrProvider: provider,
+  });
+
+  const nativeTokenPrice = await curvePoolProvider.callStatic.get_dy(
+    0,
+    1,
+    10 ** 18
+  );
+
+  return nativeTokenPrice.toString();
 }

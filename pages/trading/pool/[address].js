4db4b047e92aaf5622f005b7446dfdcd01d68644
jvalente <jvalente96@gmail.com>
2023-02-28 20:53:46 +0000

    .

--- pages/trading/pool/[address].js
+++ pages/trading/pool/[address].js
@@ -458,7 +458,7 @@ export default function TradingPool() {
                       {"(Staked positions available in the pool's "}
                       <a
                         href={"/gauges/" + poolInfo.gauge}
-                        className="text-teal-600"
+                        className="text-teal-600 underline text-sm"
                       >
                         gauge
                       </a>

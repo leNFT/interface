99e0a4917037db9cd4b9e07d77d5102e3dd13711
jvalente <jvalente96@gmail.com>
2023-04-30 22:22:52 +0100

    update curvechart

--- components/CurveChart.jsx
+++ components/CurveChart.jsx
@@ -11,25 +11,12 @@ import {
 } from "recharts";
 
 // Custom label component for the x-axis
-// Custom label component for the x-axis
 const CustomXAxisLabel = ({ viewBox }) => {
   const { x, y, width, height } = viewBox;
   return (
-    <g transform={`translate(${x},${y + height + 25})`}>
+    <g transform={`translate(${x},${y + height - 4})`}>
       <text x={width / 2 + 1} y={0} dy={0} textAnchor="middle" fill="#666">
         ◄ Sell | Buy ►
-      </text>
-    </g>
-  );
-};
-
-// Custom tick component for the x-axis
-const CustomTick = ({ x, y, payload }) => {
-  const labelText = Math.abs(payload.value);
-  return (
-    <g transform={`translate(${x},${y})`}>
-      <text x={0} y={0} dy={16} textAnchor="middle" fill="#666">
-        {labelText}
       </text>
     </g>
   );
@@ -68,14 +55,13 @@ const CurveChart = ({
         margin={{ top: 5, right: 30, left: 20, bottom: 30 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="x" tick={<CustomTick />}>
+        <XAxis dataKey="x" tickLine={false} tickFormatter={() => ""}>
           <Label
             content={<CustomXAxisLabel />}
             offset={0}
             position="insideBottom"
           />
         </XAxis>
-
         <YAxis />
         <Tooltip />
         <Line

959adc7e3bf8c3cf162f6386ea77d805c240513c
jvalente <jvalente96@gmail.com>
2023-05-13 21:05:08 +0100

    fix typo in var

--- pages/borrow.js
+++ pages/borrow.js
@@ -419,7 +419,9 @@ export default function Borrow() {
                         {isConnected
                           ? "You can use " +
                             supportedAssets.length +
-                            " of your NFTs to borrow. Just click on them!"
+                            " of your NFTs to borrow." +
+                            (supportedAssets.length > 0 &&
+                              "Just click on them!")
                           : "Connect wallet to view your NFTs."}
                       </div>
                     </Box>

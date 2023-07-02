f97f0fd192c980944bb526bb685310c70c8581e9
jvalente <jvalente96@gmail.com>
2023-07-02 19:15:28 +0100

    add genesis banner

--- components/header/HeaderController.jsx
+++ components/header/HeaderController.jsx
@@ -2,13 +2,11 @@ import BorrowHeader from "./BorrowHeader";
 import TradeHeader from "./TradeHeader";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { BannerStrip } from "@web3uikit/core";
-import { Reload, Roadmap } from "@web3uikit/icons";
 import { useAccount, useNetwork } from "wagmi";
 import { useSwitchNetwork } from "wagmi";
 import Router from "next/router";
-import { Button } from "grommet";
 import { useState } from "react";
-import BedtimeIcon from "@mui/icons-material/Bedtime";
+//import BedtimeIcon from "@mui/icons-material/Bedtime";
 import { useRouter } from "next/router";
 var mode;
 
@@ -33,28 +31,47 @@ export default function HeaderController() {
     }
   };
 
-  console.log("mode", mode);
-
   return (
-    <div className="p-4 pb-0 md:pb-2 mb-2 border-b-2 flex flex-row justify-between items-center">
-      <div className="flex flex-row-reverse md:flex-row items-center">
-        <div className="flex flex-row-reverse md:flex-row 2xl:flex-col items-center">
-          {mode == "/borrow" ? <BorrowHeader /> : <TradeHeader />}
-        </div>
+    <div>
+      <div
+        onClick={() => {
+          Router.push("/genesis");
+        }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: "#4caf50", // You can customize this
+          color: "white", // You can customize this
+          textAlign: "center",
+          padding: "6px", // You can customize this
+          zIndex: 999, // Make sure the banner always on top
+          cursor: "pointer",
+        }}
+      >
+        Genesis NFT mint is live!
       </div>
-      <div className="flex flex-row space-x-4 items-center px-8">
-        {/* <Button
+      <div className="p-4 pb-0 md:pb-2 my-2 border-b-2 flex flex-row justify-between items-center">
+        <div className="flex flex-row-reverse md:flex-row items-center">
+          <div className="flex flex-row-reverse md:flex-row 2xl:flex-col items-center">
+            {mode == "/borrow" ? <BorrowHeader /> : <TradeHeader />}
+          </div>
+        </div>
+        <div className="flex flex-row space-x-4 items-center px-8">
+          {/* <Button
             size="small"
             onClick={() => {
               toggleDarkMode();
             }}
             icon={<BedtimeIcon />}
           /> */}
-        <ConnectButton
-          showBalance={false}
-          chainStatus={{ smallScreen: "none", largeScreen: "icon" }}
-          accountStatus={{ smallScreen: "none", largeScreen: "none" }}
-        />
+          <ConnectButton
+            showBalance={false}
+            chainStatus={{ smallScreen: "none", largeScreen: "icon" }}
+            accountStatus={{ smallScreen: "none", largeScreen: "none" }}
+          />
+        </div>
       </div>
     </div>
   );
